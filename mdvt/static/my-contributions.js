1636dd079cfd2bd3e6a12355ca92d4b802d121d4
Gabriel Lee <chihonglee777@gmail.com>
2020-07-19 15:34:57 +0800

    My Contribution page now reads depict value from db

--- mdvt/static/my-contributions.js
+++ mdvt/static/my-contributions.js
@@ -1,24 +1,42 @@
 $('.contribution-card').each(function() {
     var claim_label = $(this).find('.claim-id');
-    var claim_link = $(this).find('.claim-link');
-    var entity_id = claim_label.text();
-    $.get("https://commons.wikimedia.org/w/api.php", {
-    	"action": "wbgetclaims",
-    	"format": "json",
-        "claim": entity_id,
-        "origin": "*"
-    }, function(claim_data) {
-        var claim_id = claim_data.claims.P180[0].mainsnak.datavalue.value.id;
-        claim_link.attr("href", "https://www.wikidata.org/wiki/" + claim_id);
-        $.get("https://www.wikidata.org/w/api.php", {
-        	"action": "wbgetentities",
+    if (claim_label.length) {
+        var claim_link = $(this).find('.claim-link');
+        var entity_id = claim_label.text();
+        $.get("https://commons.wikimedia.org/w/api.php", {
+            "action": "wbgetclaims",
         	"format": "json",
-        	"ids": claim_id,
+            "claim": entity_id,
             "origin": "*"
-        }, function(entity_data) {
-            claim_label.html(entity_data.entities[claim_id].labels.en.value);
+        }, function(claim_data) {
+            var claim_id = claim_data.claims.P180[0].mainsnak.datavalue.value.id;
+            claim_link.attr("href", "https://www.wikidata.org/wiki/" + claim_id);
+            $.get("https://www.wikidata.org/w/api.php", {
+                "action": "wbgetentities",
+                "format": "json",
+                "ids": claim_id,
+                "origin": "*"
+            }, function(entity_data) {
+                claim_label.html(entity_data.entities[claim_id].labels.en.value);
+            });
         });
-    });
+    }
+
+    var depict_value_label = $(this).find('.depict-value');
+    if (depict_value_label.length) {
+        var claim_link = $(this).find('.claim-link');
+        var depict_value = depict_value_label.text().trim();
+        claim_link.attr("href", "https://www.wikidata.org/wiki/" + depict_value);
+        $.get("https://www.wikidata.org/w/api.php", {
+            "action": "query",
+            "format": "json",
+            "prop": "pageterms",
+            "titles": depict_value,
+            "origin": "*"
+        }, function(response) {
+            depict_value_label.html(Object.values(response.query.pages)[0].terms.label[0]);
+        });
+    }
 
     var img = $(this).find('.card-img-top');
     var img_link = $(this).find('.img-link');
