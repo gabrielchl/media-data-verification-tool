849372dab55123fc6bcd148cc7494a11519f95b3
Gabriel Lee <chihonglee777@gmail.com>
2020-07-22 19:32:54 +0800

    Add first test German translation string (flask babel experiment)

--- mdvt/__init__.py
+++ mdvt/__init__.py
@@ -1,14 +1,41 @@
-from flask import Flask
+from flask import Flask, session
+from flask_babel import Babel
 from flask_sqlalchemy import SQLAlchemy
 
 from mdvt.config.config import config
+
+import os
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = config['SECRET_KEY']
 app.config['SQLALCHEMY_DATABASE_URI'] = config['DATABASE_URI']
 app.config['JSON_SORT_KEYS'] = False
+app.config['BABEL_TRANSLATION_DIRECTORIES'] = os.getcwd() + '/translations'
+
 db = SQLAlchemy(app)
+
+babel = Babel(app)
+from mdvt.database.util import db_get_user_setting
+
+
+@babel.localeselector
+def get_locale():
+    locale = db_get_user_setting(session['user_id'], 'locale')
+    if locale:
+        return locale
+    else:
+        return 'en_UK'
+
+
+@babel.timezoneselector
+def get_timezone():
+    timezone = db_get_user_setting(session['user_id'], 'timezone')
+    if timezone:
+        return timezone
+    else:
+        return 'Etc/UTC'
+
 
 from mdvt.contribute.route import contribute_bp
 from mdvt.main.route import main_bp
