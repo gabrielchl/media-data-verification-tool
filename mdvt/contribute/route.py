9861119acff0a7b335b32cae4acc45bc6bb9cead
Gabriel Lee <chihonglee777@gmail.com>
2020-07-30 14:52:46 +0800

    Added (basic) filters to contributions and users public API

--- mdvt/contribute/route.py
+++ mdvt/contribute/route.py
@@ -152,13 +152,31 @@ def api_contribute():
 
 @contribute_bp.route('/api/query/contributions')
 def api_public_query_contrib():
-    contribs = db.session.query(Contribution, Question).join(Question).all()
+    contribs = db.session.query(Contribution, Question).join(Question)
+
+    question_filters = ['page_id', 'type', 'claim_id',
+                        'depict_value', 'qualifier_value']
+    for question_filter in question_filters:
+        filter_value = request.args.get(question_filter)
+        if filter_value:
+            contribs = contribs.filter(
+                       getattr(Question, question_filter) == filter_value)
+
+    contrib_filters = ['user_id']
+    for contrib_filter in contrib_filters:
+        filter_value = request.args.get(contrib_filter)
+        if filter_value:
+            contribs = contribs.filter(
+                       getattr(Contribution, contrib_filter) == filter_value)
+
+    contribs = contribs.all()
     ret = []
     for contrib in contribs:
         ret.append({
             'id': contrib[0].id,
             'user_id': contrib[0].user_id,
             'time_created': datetime.timestamp(contrib[0].time_created),
+            'page_id': contrib[1].page_id,
             'type': contrib[1].type,
             'depict_value': contrib[1].depict_value,
             'qualifier_value': contrib[1].qualifier_value,
@@ -167,9 +185,19 @@ def api_public_query_contrib():
     return jsonify(ret)
 
 
+# TODO: get contrib score in 1 query
 @contribute_bp.route('/api/query/users')
 def api_public_query_user():
-    users = User.query.all()
+    users = User.query
+
+    filters = ['sul_id']
+    for filter in filters:
+        filter_value = request.args.get(filter)
+        if filter_value:
+            users = users.filter(
+                       getattr(User, filter) == filter_value)
+
+    users = users.all()
     ret = []
     for user in users:
         ret.append({
