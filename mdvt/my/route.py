28fb188af422c33292631c74865e912af774e993
Gabriel Lee <chihonglee777@gmail.com>
2020-10-24 15:38:16 +0100

    Fix error where stats show in my contributions page is wrong
    It was showing a global contributions count instead of count for
    the user.

--- mdvt/my/route.py
+++ mdvt/my/route.py
@@ -10,11 +10,12 @@ my_bp = Blueprint('my', __name__)
 
 @my_bp.route('/my/contributions')
 def contributions():
-    return render_template('my/contributions.html',
-                           title='My Contributions',
-                           username=session.get('username', None),
-                           stat_contributions=get_contrib_count(),
-                           stat_tests=get_test_contrib_count(),
-                           stat_all_score=get_test_contrib_score(),
-                           stat_10_score=get_test_contrib_score(count=10),
-                           contributions=get_contributions())
+    return render_template(
+        'my/contributions.html',
+        title='My Contributions',
+        username=session.get('username', None),
+        stat_contributions=get_contrib_count(session.get('user_id', None)),
+        stat_tests=get_test_contrib_count(session.get('user_id', None)),
+        stat_all_score=get_test_contrib_score(session.get('user_id', None)),
+        stat_10_score=get_test_contrib_score(session.get('user_id', None), 10),
+        contributions=get_contributions())
